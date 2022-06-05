0fbf0703d5b8d8b0ffd0d359dbae48add02537e8
Robert Soriano <sorianorobertc@gmail.com>
2022-06-05 16:24:03 -0700

    docs: add README

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
-# TODO
+# nuxt-web3.js
+
+[![Version](https://img.shields.io/npm/v/nuxt-web3.js?style=flat&colorA=000000&colorB=000000)](https://www.npmjs.com/package/nuxt-web3.js)
+
+Nuxt 3 module for Web3.js.
+
+## Installation
+
+```bash
+npm install nuxt-web3.js
+```
+
+## Usage
+
+```ts
+export default defineNuxtConfig({
+  modules: ['nuxt-web3.js'],
+})
+```
+
+Custom provider
+
+```html
+<script setup lang="ts">
+const $Web3 = useWeb3()
+
+const provider = new $Web3.providers.HttpProvider('PROVIDER_HOST')
+const web3 = new $Web3(provider)
+</script>
+```
+
+Browser or window Ethereum provider
+
+```html
+<script setup lang="ts">
+const $Web3 = useWeb3()
+
+const provider = new $Web3.providers.HttpProvider('PROVIDER_HOST')
+const web3 = new $Web3(provider)
+
+// or any wallet that inject an Ethereum Provider into the browser or window
+if (process.client) {
+  const web3 = new $Web3(window.ethereum)
+}
+</script>
+```
+
+## License
+
+MIT
--- playground/pages/index.vue
+++ playground/pages/index.vue
@@ -1,30 +1,12 @@
 <script setup lang="ts">
 const $Web3 = useWeb3()
 const runtimeConfig = useRuntimeConfig()
-console.log(runtimeConfig.public.provider.host)
-const ethEnabled = async () => {
-  try {
-    const walletAddress = '0x57a44b7942bc7B512BE4A1E7dA3d4F0705212453'
 
-    const web3 = new $Web3(new $Web3.providers.HttpProvider(runtimeConfig.public.provider.host))
-    // const web3 = new $Web3((window as any).ethereum)
-    const balance = await web3.eth.getBalance(walletAddress) // Will give value
-    return balance
-  }
-  catch (e) {
-    console.log('Error', e)
-  }
-}
+const walletAddress = '0x57a44b7942bc7B512BE4A1E7dA3d4F0705212453'
 
-onMounted(async () => {
-  if (window.hasOwnProperty('ethereum')) {
-    const result = await ethEnabled()
-    console.log(result)
-  }
-})
-
-const result = await ethEnabled()
-console.log(result)
+const web3 = new $Web3(new $Web3.providers.HttpProvider(runtimeConfig.public.provider.host))
+// const web3 = new $Web3((window as any).ethereum)
+const balance = await web3.eth.getBalance(walletAddress) // Will give value
 </script>
 
 <template>
