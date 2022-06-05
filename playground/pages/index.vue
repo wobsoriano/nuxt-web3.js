748b0fe13190068d8b92e9cb5f22a0fec20ab223
Robert Soriano <sorianorobertc@gmail.com>
2022-06-05 16:45:15 -0700

    chore(playground): update sample

--- playground/pages/index.vue
+++ playground/pages/index.vue
@@ -5,12 +5,12 @@ const runtimeConfig = useRuntimeConfig()
 const walletAddress = '0x57a44b7942bc7B512BE4A1E7dA3d4F0705212453'
 
 const web3 = new $Web3(new $Web3.providers.HttpProvider(runtimeConfig.public.provider.host))
-// const web3 = new $Web3((window as any).ethereum)
-const balance = await web3.eth.getBalance(walletAddress) // Will give value
+
+const { data: balance } = await useAsyncData('balance', () => web3.eth.getBalance(walletAddress))
 </script>
 
 <template>
   <div>
-    hello world
+    Balance: {{ balance }}
   </div>
 </template>
