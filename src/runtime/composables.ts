22f809ae2368eb3e7d185651b1a82264ceb31184
Robert Soriano <sorianorobertc@gmail.com>
2022-06-20 10:33:35 -0700

    refactor: remove redundant composables

--- src/runtime/composables.ts
+++ src/runtime/composables.ts
@@ -4,25 +4,6 @@ import { useNuxtApp } from '#app'
 
 export function useWeb3(): typeof Web3 {
   const { $Web3 } = useNuxtApp()
+  // @ts-expect-error: Internal
   return $Web3
-}
-
-export function useWeb3Utils(): typeof Web3.utils {
-  const { $utils } = useNuxtApp()
-  return $utils
-}
-
-export function useWeb3Providers(): typeof Web3.providers {
-  const { $providers } = useNuxtApp()
-  return $providers
-}
-
-export function useWeb3Modules(): typeof Web3.modules {
-  const { $modules } = useNuxtApp()
-  return $modules
-}
-
-export function useWeb3GivenProvider(): typeof Web3.givenProvider {
-  const { $givenProvider } = useNuxtApp()
-  return $givenProvider
 }
