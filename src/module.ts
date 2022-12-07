faf8b13db8d89f63be98962cc38efad91f476ff1
wobsoriano <sorianorobertc@gmail.com>
2022-12-07 15:13:18 -0800

    remove unused polyfills

--- src/module.ts
+++ src/module.ts
@@ -1,8 +1,6 @@
 import { fileURLToPath } from 'url'
 import { resolve } from 'pathe'
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
-import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill'
-import { addImports, addPlugin, defineNuxtModule, extendViteConfig, useLogger } from '@nuxt/kit'
+import { addImports, addPlugin, defineNuxtModule, useLogger } from '@nuxt/kit'
 import Web3 from 'web3'
 
 const logger = useLogger('nuxt-web3.js')
@@ -20,24 +18,6 @@ export default defineNuxtModule({
     nuxt.hook('vite:extendConfig', (config) => {
       config.define = config.define || {}
       config.define['process.env.NODE_DEBUG'] = JSON.stringify(process.env.NODE_DEBUG)
-    })
-
-    extendViteConfig((config) => {
-      config.optimizeDeps = config.optimizeDeps || {}
-      config.optimizeDeps.esbuildOptions = config.optimizeDeps.esbuildOptions || {}
-      config.optimizeDeps.esbuildOptions.define = config.optimizeDeps.esbuildOptions.define || {}
-
-      // Node.js global to browser globalThis
-      config.optimizeDeps.esbuildOptions.define.global = 'globalThis'
-
-      // Enable esbuild polyfill plugins
-      config.optimizeDeps.esbuildOptions.plugins?.push(
-        NodeGlobalsPolyfillPlugin({
-          process: true,
-          buffer: true,
-        }),
-        NodeModulesPolyfillPlugin(),
-      )
     })
 
     addImports([
